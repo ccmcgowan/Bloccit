f9d6b267d2176cb2ca736a480cdb86220b525396
Cody McGowan <cody.mcgowan@gmail.com>
2016-08-23 13:28:51 -0700

    updated assignment_28

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:Question) { Question.create!(title: "New Question Title", body: "New Post Body", boolean: false) }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Post Body", boolean: false) }
 
   describe "attributes" do
     it "has a title attribute" do
       expect(question).to have_attributes(title: "Question Title")
+      # question = Question.new
+      # question.title = "New question"
+      # expect(question.title).to eq("New question")
     end
+
+    # it "has a title attribute that can be saved" do
+    #   question = Question.new
+    #   question.title = "New question"
+    #   question.save
+    #   reloaded_question = Question.first
+    #   expect(reloaded_question.title).to eq("New question")
+    # end
 
     it "has a body attribute" do
       expect(question).to have_attributes(body: "Question Body")
@@ -14,5 +25,6 @@ RSpec.describe Question, type: :model do
 
     it "has a resolved attribute" do
       expect(question).to have_attributes(boolean: false)
+    end
   end
 end
